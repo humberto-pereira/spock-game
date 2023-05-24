ba74a45c8471b0f6a972333b63b9c384273c220b
Humberto Pereira <humberto_pereira@live.com>
2023-05-24 12:31:41 +0000

    Add game conditionals to check who is the winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,83 @@
 //wait for the DOM loading before running the game
 //get the button elements and add event listeners to them
 
-//snipet from love-maths
+//based on love-maths code
 document.addEventListener("DOMContentLoaded", function() {
     let clickedButtons = document.getElementsByTagName("button");
     for (let button of clickedButtons) {
-        button.addEventListener("click", function(){
+        function playerChoiceFunction () {
+            button.addEventListener("click", function(){
             let playerChoice = this.getAttribute("data-type");
             checkWinner(playerChoice);
-        })      
+            })
+        }      
     }
+    checkWinner();
 })
+
+function generateComputerChoice() {
+    let computerChoice = ["rock-computer", "lizard-computer", "paper-computer", "scissors-computer", "spock-computer"];
+    let i = Math.floor(Math.random() * computerChoice.length);
+    computerChoice = computerChoice[i];
+    checkWinner(computerChoice);
+}
+
+function checkWinner(playerChoice, computerChoice) {
+
+    if (playerChoice === "scissors-player" &&  computerChoice === "rock-computer") {
+        alert("computer win: Rock crushes scissors");
+    } else if (playerChoice === "rock-player" && computerChoice === "scissors-computer") {
+        alert("player win: Rock crushes scissors");
+
+    } else if (playerChoice === "paper-player" && computerChoice === "scissors-computer") {
+        alert("computer win: Scissors cut paper");
+    } else if (playerChoice === "scissors-player" && computerChoice === "paper-computer") {
+        alert("player win: Scissors cut paper");
+    
+    } else if (playerChoice === "rock-player" && computerChoice === "paper-computer") {
+        alert("computer win: Paper covers rock");
+    } else if (playerChoice === "paper-player" && computerChoice === "rock-computer") {
+        alert("player win: Paper covers rock");
+    
+    } else if (playerChoice === "lizard-player" && computerChoice === "rock-computer") {
+        alert("computer win: rock crushes lizard");
+    } else if (playerChoice === "rock-player" && computerChoice === "lizard-computer") {
+        alert("player win: rock crushes lizard");
+    
+    } else if (playerChoice === "spock-player" && computerChoice === "lizard-computer") {
+        alert("computer win: Lizard poisons Spock");
+    } else if (playerChoice === "lizard-player" && computerChoice === "spock-computer") {
+        alert("player win: Lizard poisons Spock");
+    
+    } else if (playerChoice === "scissors-player" && computerChoice === "spock-computer") {
+        alert("computer win: Spock smashes scissors");
+    } else if (playerChoice === "spock-player" && computerChoice === "scissors-computer") {
+        alert("player win: Spock smashes scissors");
+    
+    } else if (playerChoice === "lizard-player" && computerChoice === "scissors-computer") {
+        alert("computer win: Scissors decapitate lizard");
+    } else if (playerChoice === "scissors-player" && computerChoice === "lizard-computer") {
+        alert("player win: Scissors decapitate lizard");
+    
+    } else if (playerChoice === "paper-player" && computerChoice === "lizard-computer") {
+        alert("computer win: Lizard eats paper");
+    } else if (playerChoice === "lizard-player" && computerChoice === "paper-computer") {
+        alert("player win: Lizard eats paper");
+    
+    } else if (playerChoice === "spock-player" && computerChoice === "paper-computer") {
+        alert("computer win: Paper disproves Spock");
+    } else if (playerChoice === "paper-player" && computerChoice === "spock-computer") {
+        alert("player win: Paper disproves Spock");
+    
+    } else if (playerChoice === "rock-player" && computerChoice === "spock-computer") {
+        alert("computer win: Spock vaporizes rock");
+    } else if (playerChoice === "spock-player" && computerChoice === "rock-computer") {
+        alert("player win: Spock vaporizes rock");
+    
+    } else {
+        alert("No winner: it's a tie");
+    }
+        
+}
+
 
